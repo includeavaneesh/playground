29e1b723cd5274e8f8802696375f2b3e8ad96993
includeavaneesh <avaneeshkanshi5@gmail.com>
2024-05-31 11:06:14 -0400

    rabbit mq

---
+++ rabbitmq-demo/src/main/java/com/playground/Routing/RecvLogsDirect.java
@@ -0,0 +1,32 @@
+package com.playground.Routing;
+
+import com.rabbitmq.client.*;
+
+public class RecvLogsDirect {
+
+    private static final String EXCHANGE_NAME = "topic_logs";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "topic");
+        String queueName = channel.queueDeclare().getQueue();
+
+        String [] argv = new String[]{"kern.*","*.critical"};
+
+        for (String severity : argv) {
+            channel.queueBind(queueName, EXCHANGE_NAME, severity);
+        }
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" +
+                    delivery.getEnvelope().getRoutingKey() + "':'" + message + "'");
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
+    }
+}
---
+++ rabbitmq-demo/src/main/java/com/playground/Topic/EmitLogTopic.java
@@ -0,0 +1,4 @@
+package com.playground.Topic;
+
+public class EmitLogTopic {
+}
